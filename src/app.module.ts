70c90eecabab676061e7da7b805734a7fb2958dc
Pasinee Rukpeomrach <64160056@go.buu.ac.th>
2023-02-05 20:56:56 +0700

    setup TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './products/entities/product.entity';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      synchronize: true,
+      logging: false,
+      entities: [User, Product],
+      migrations: [],
+      subscribers: [],
+    }),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
