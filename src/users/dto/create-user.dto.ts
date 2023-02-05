150176108184b2d802a78c3a25c0e933fc5c3e74
Pasinee Rukpeomrach <64160056@go.buu.ac.th>
2023-02-05 22:35:20 +0700

    validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,17 @@
+import { IsNotEmpty, Length, Matches } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @Length(4, 16)
   login: string;
 
+  @IsNotEmpty()
+  @Length(4, 16)
   name: string;
 
+  @IsNotEmpty()
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+  )
   password: string;
 }
