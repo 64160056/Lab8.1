aad31a32267ce18b2ce651f266571b157dd3b5c3
Pasinee Rukpeomrach <64160056@go.buu.ac.th>
2023-02-05 21:27:26 +0700

    finish Setup UserInsert SelectAll

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,7 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  login: string;
+
+  name: string;
+
+  password: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const user: User = new User();
+    user.login = createUserDto.login;
+    user.name = createUserDto.name;
+    user.password = createUserDto.password;
+    return this.usersRepository.save(user);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
   }
 
   findOne(id: number) {
